5c6a1d1dbbcaa28ff385e6f5d5fab9873c979c24
JethroAlbano <skyjeth1371@gmail.com>
2017-09-03 16:20:44 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,16 +1,10 @@
 <?php
-if(isset($_POST['submit']))
-	{
+if(isset($_POST['log-out']))
+{
 	session_start();
 	$_SESSION['logined'] = False;
-	header("Location:homepage.html);
-	/*
-	if($_SESSION['who'] == 'admin'){
-		header("Location: admin_login.html");
-	}else{
-		header("Location: user_login.html");
-	}*/
+	header("Location: homepage.html");
 	session_destroy();
-	die();
+    die();
 }
 ?>
