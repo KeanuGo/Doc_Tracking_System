9d0f87350838b1361b5c23b8f3e39c06e27a9518
KeanuGo <keanuguanezogo@yahoo.com>
2017-11-15 18:56:26 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,6 +2,18 @@
 if(isset($_POST['log-out']))
 {
 	session_start();
+	define('DB_HOST', 'localhost');
+	define('DB_NAME', 'document_tracking_system');
+	define('DB_USER','root');
+	define('DB_PASSWORD','');
+	$con=new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+	if ($con->connect_errno) {
+		echo "Failed to connect to MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
+	}
+	$updated_by = $_SESSION['username'];
+	$data3_2 = mysqli_query($con, "update users set active='offline' where username= '$updated_by'");
+	
+	
 	$_SESSION['logined'] = False;
 	
 	header("Location: homepage.html");
