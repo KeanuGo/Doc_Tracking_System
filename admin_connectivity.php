f439ac72735d6105089969fc5d070453541ffe73
JethroAlbano <skyjeth1371@gmail.com>
2017-11-18 10:42:45 +0800

    Update admin_connectivity.php

--- admin_connectivity.php
+++ admin_connectivity.php
@@ -2,7 +2,8 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'document_tracking_system');
 define('DB_USER','root');
-define('DB_PASSWORD','');
+define('DB_PASSWORD','Jethshanroyce1204');
+
 $con=new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 if ($con->connect_errno) {
     echo "Failed to connect to MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
@@ -30,7 +31,8 @@ function SignIn()
 		}
 		else
 		{
-			header("Location:homepage.html");
+			echo '<center><text style = "font-family: calibri"> Username Does Not Match Password. </text></center>';
+			header("Refresh:2; URL=homepage.html");
 		}
 	}else{
 		header("Location:homepage.html");
